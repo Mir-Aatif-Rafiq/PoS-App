bd8d2fa39625a631a6c3a4aa11df73aca1bdf4c6
mir-aatif-rafiq <bitemiraatif@gmail.com>
2025-02-24 09:22:58 +0530

    [INSERT] UserPrincipal.util : NA

---
+++ src/main/java/com/pos/app/util/UserPrincipal.java
@@ -0,0 +1,22 @@
+package com.pos.app.util;
+
+public class UserPrincipal {
+    private int id;
+    private String email;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
