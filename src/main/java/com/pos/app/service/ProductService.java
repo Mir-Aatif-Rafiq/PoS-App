73dd6aa1b7025ef115630cc56a01291670b80a50
mir-aatif-rafiq <bitemiraatif@gmail.com>
2025-02-24 09:06:32 +0530

    [BUILD] ProductService : API for ProductService

---
+++ src/main/java/com/pos/app/service/ProductService.java
@@ -0,0 +1,42 @@
+package com.pos.app.service;
+
+import com.pos.app.dao.ProductDao;
+import com.pos.app.pojo.ProductPojo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+@Service
+public class ProductService {
+    @Autowired
+    public ProductDao pd;
+
+    @Transactional
+    public void insert(ProductPojo pp){
+        pd.insert(pp);
+        return;
+    }
+    @Transactional
+    public ProductPojo get(int id){
+        return pd.select(id);
+    }
+    @Transactional
+    public List<ProductPojo> getAll(){
+        return pd.selectAll();
+    }
+    @Transactional
+    public void update(int id, ProductPojo new_pp){
+        pd.update(id,new_pp);
+        return;
+    }
+    @Transactional
+    public void delete(int id){
+        pd.delete(id);
+        return;
+    }
+
+
+}
