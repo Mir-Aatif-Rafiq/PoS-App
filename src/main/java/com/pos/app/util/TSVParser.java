7a3ea07f73ff9ff096597ff1373a2f875799ef15
mir-aatif-rafiq <bitemiraatif@gmail.com>
2025-03-23 23:59:51 +0530

    [REFACTOR] /util/* : WORKING

--- src/main/java/com/pos/app/util/TSVParser.java
+++ src/main/java/com/pos/app/util/TSVParser.java
@@ -1,33 +1,21 @@
 package com.pos.app.util;
 
-import com.pos.app.pojo.ProductPojo;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.List;
 
+import java.util.stream.Collectors;
+
 public class TSVParser {
-    public static List<ProductPojo> parseTsvFile(MultipartFile file) throws IOException {
-        List<ProductPojo> productList = new ArrayList<>();
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(file.getInputStream()))) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                String[] fields = line.split("\t");
-                if (fields.length >= 6) {
-                    ProductPojo product = new ProductPojo();
-                    product.setProductName(fields[0]);
-                    product.setProductBarcode(Integer.parseInt(fields[1]));
-                    product.setProductPrice(Double.parseDouble(fields[2]));
-                    product.setProductQuantity(Integer.parseInt(fields[3]));
-                    product.setClientId(Integer.parseInt(fields[4]));
-                    product.setProductImageLink(fields[5]);
-                    productList.add(product);
-                }
-            }
-        }
-        return productList;
+
+    public static Boolean checkTotalLines(MultipartFile file, Integer maxRows) throws IOException {
+        List<String> lines = new BufferedReader(new InputStreamReader(file.getInputStream()))
+                .lines()
+                .collect(Collectors.toList());
+        return lines.size() > maxRows;
     }
 }
+
